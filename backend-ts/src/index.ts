9579dc1739d11a1e679d508f6025a93d9542c894
joshu-git <argentjackjoshua@outlook.com>
2025-12-08 20:24:19 +0000

    Update index.ts to allow player routes
    Means frontend can call these routes

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -2,18 +2,32 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+//Gives access to player routes
+import playersRoutes from "./routes/players.routes.js"
+
 //Load environment variables
 dotenv.config();
 
 //Create the Express app
 const app = express();
 
-//Setup routes here
+//Parses JSON bodies
+app.use(express.json())
 
 //Enable CORS for the frontend URL
 app.use(cors({
     origin: process.env.FRONTEND_URL,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+//All routes for players and matches
+app.use("/players", playersRoutes);
+
+//Health check to confirm the backend is running
+app.get("/", (_req, res) => {
+    res.json({ message: "Backend is running" });
+});
 
 //Start server using Render Port or local dev
 const Port = process.env.PORT || 5000;
